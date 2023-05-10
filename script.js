17a46b17b3ee1907ef565972303b9d49cea882a5
Zhosas <lundholm00@hotmail.com>
2023-05-10 11:26:29 +0200

    En masse Javascript på bade

---
+++ script.js
@@ -0,0 +1,99 @@
+// JavaScript Document
+
+function indOvn() {
+	document.getElementById("bIndOvn").style.background = "white";
+	document.getElementById("bIndOvn").style.color = "#A68876";
+	document.getElementById("bUdOvn").style.background = "#A68876";
+	document.getElementById("bUdOvn").style.color = "white";
+}
+
+function udOvn() {
+	document.getElementById("bUdOvn").style.background = "white";
+	document.getElementById("bUdOvn").style.color = "#A68876";
+	document.getElementById("bIndOvn").style.background = "#A68876";
+	document.getElementById("bIndOvn").style.color = "white";
+}
+
+function ingenD() {
+	document.getElementById("bIngenD").style.background = "white";
+	document.getElementById("bIngenD").style.color = "#A68876";
+	document.getElementById("bLuftD").style.background = "#A68876";
+	document.getElementById("bLuftD").style.color = "white";
+	document.getElementById("bVandD").style.background = "#A68876";
+	document.getElementById("bVandD").style.color = "white";
+	document.getElementById("bBeggeD").style.background = "#A68876";
+	document.getElementById("bBeggeD").style.color = "white";
+}
+
+function luftD() {
+	document.getElementById("bIngenD").style.background = "#A68876";
+	document.getElementById("bIngenD").style.color = "white";
+	document.getElementById("bLuftD").style.background = "white";
+	document.getElementById("bLuftD").style.color = "#A68876";
+	document.getElementById("bVandD").style.background = "#A68876";
+	document.getElementById("bVandD").style.color = "white";
+	document.getElementById("bBeggeD").style.background = "#A68876";
+	document.getElementById("bBeggeD").style.color = "white";
+}
+
+function vandD() {
+	document.getElementById("bIngenD").style.background = "#A68876";
+	document.getElementById("bIngenD").style.color = "white";
+	document.getElementById("bLuftD").style.background = "#A68876";
+	document.getElementById("bLuftD").style.color = "white";
+	document.getElementById("bVandD").style.background = "white";
+	document.getElementById("bVandD").style.color = "#A68876";
+	document.getElementById("bBeggeD").style.background = "#A68876";
+	document.getElementById("bBeggeD").style.color = "white";
+}
+
+function beggeD() {
+	document.getElementById("bIngenD").style.background = "#A68876";
+	document.getElementById("bIngenD").style.color = "white";
+	document.getElementById("bLuftD").style.background = "#A68876";
+	document.getElementById("bLuftD").style.color = "white";
+	document.getElementById("bVandD").style.background = "#A68876";
+	document.getElementById("bVandD").style.color = "white";
+	document.getElementById("bBeggeD").style.background = "white";
+	document.getElementById("bBeggeD").style.color = "#A68876";
+}
+
+function brunTræ() {
+	document.getElementById("bBrunT").style.border = "2px solid #1D5FE1";
+	document.getElementById("bSortT").style.border = "2px solid black";
+}
+
+function sortTræ() {
+	document.getElementById("bBrunT").style.border = "2px solid #FDBC7B";
+	document.getElementById("bSortT").style.border = "2px solid #1D5FE1";
+}
+
+function karHvid() {
+	document.getElementById("bKarH").style.border = "2px solid #1D5FE1";
+	document.getElementById("bKarS").style.border = "2px solid black";
+	document.getElementById("bKarG").style.border = "2px solid grey";
+	document.getElementById("bKarR").style.border = "2px solid #A12A2C";
+}
+
+function karSort() {
+	document.getElementById("bKarH").style.border = "2px solid #EAEAEA";
+	document.getElementById("bKarS").style.border = "2px solid #1D5FE1";
+	document.getElementById("bKarG").style.border = "2px solid grey";
+	document.getElementById("bKarR").style.border = "2px solid #A12A2C";
+}
+
+function karGrå() {
+	document.getElementById("bKarH").style.border = "2px solid #EAEAEA";
+	document.getElementById("bKarS").style.border = "2px solid black";
+	document.getElementById("bKarG").style.border = "2px solid #1D5FE1";
+	document.getElementById("bKarR").style.border = "2px solid #A12A2C";
+}
+
+function karRød() {
+	document.getElementById("bKarH").style.border = "2px solid #EAEAEA";
+	document.getElementById("bKarS").style.border = "2px solid black";
+	document.getElementById("bKarG").style.border = "2px solid grey";
+	document.getElementById("bKarR").style.border = "2px solid #1D5FE1";
+}
+
+
